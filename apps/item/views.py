568dec5ecda09d141e993267a160062f6c472905
gersonalex <alex.gerson@hotmail.com>
2020-08-20 17:10:00 -0400

    Versionado de item y arreglo de adjuntar archivo

--- apps/item/views.py
+++ apps/item/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
 from django.db.models import Q
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render,redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DeleteView, UpdateView
 from guardian.mixins import PermissionRequiredMixin
