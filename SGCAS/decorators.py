329806e30d8320ac3104a26eb1a3b9330af244a4
gersonalex <alex.gerson@hotmail.com>
2020-09-04 22:41:43 -0400

    permisos por fase al 100%, falta incluir los decoradores

--- SGCAS/decorators.py
+++ SGCAS/decorators.py
@@ -2,18 +2,24 @@ from django.http import HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
 from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
+from apps.rol.models import Rol
+from apps.fase.models import Fase
 
 ##Checkea los permisos por fase
 def requiere_permiso(permiso):
     def decorator(view_func):
         def wrap(request, *args, **kwargs):
-            for rol_fase in Rol.objects.filter(fase=get_object_or_404(Fase, pk=kwargs.get('id_fase'))):
-                if request.user in rol_fase.usuarios.all() and permiso in rol_fase.group.permissions.all():
-                    return view_func(request, *args, **kwargs)
-            raise PermissionDenied
+            query_rol=Rol.objects.filter(fase=get_object_or_404(Fase, pk=kwargs.get('id_fase')))
+            for rol_fase in query_rol:
+                if request.user in rol_fase.usuarios.all():
+                    try:
+                        if rol_fase.group.permissions.get(name=permiso):
+                            return view_func(request, *args, **kwargs)
+                    except:
+                        raise PermissionDenied
         return wrap
     return decorator
-    
+
 
 def administrator_only(view_func):
     """
