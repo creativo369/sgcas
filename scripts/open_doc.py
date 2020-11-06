38d3b4c5c6d722efade0742ab61716fa25fb4d0c
Guille-Gimenez <gimenezguillermo186@gmail.com>
2020-11-06 10:27:58 -0300

    corrigiendo mis rutas

--- scripts/open_doc.py
+++ scripts/open_doc.py
@@ -1,6 +1,6 @@
 import webbrowser
 new = 2 # open in a new tab, if possible
 
-url = "/home/victor/SGCAS/apps/docs-index/index.html"
+url = "/home/guillermo/SGCAS/apps/docs-index/index.html"
 webbrowser.open(url,new=new)
 
